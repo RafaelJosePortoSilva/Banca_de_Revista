52bb0beeba9f835e53fc804e7de113b0dca30468
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2023-10-12 23:04:42 -0300

    add input

--- src/Aplicacao/Main.java
+++ src/Aplicacao/Main.java
@@ -1,18 +1,33 @@
 package Aplicacao;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
 
-        Scanner escolha = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int escolha;
+        Boolean ativa;
 
-        System.out.println(" " +
+        System.out.println("" +
                 "Bem vindo(a) à Banca.\n" +
-                "Escolha ums das operações abaixo:" +
-                "[1] Consultar estoque" +
-                "[2] Adicionar produto" +
+                "Escolha ums das operações abaixo:\n" +
+                "\n" +
+                "[1] Consultar estoque\n" +
+                "[2] Adicionar produto\n" +
                 "[3] Operar estoque");
+        ativa = true;
+        while(true) {
+            try {
+                escolha = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Digite um número");
+                ativa = false;
+            }
+            if(ativa){break;}
+        }
+
 
 
     }
